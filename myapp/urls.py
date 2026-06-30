3ef832bfff7d2784e8f73f2c3f71a1451cc71dc0
Phubet <lomabut33@gmail.com>
2026-06-30 15:22:49 +0700

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from myapp import views 
+
+urlpatterns = [
+    path('', views.index, name='index'), # ผูกเข้ากับฟังก์ชัน index ใน views.py
+    path('about/', views.about, name='about'), # ผูกเข้ากับฟังก์ชัน about ใน views.py
+    path('contact/', views.contact, name='contact'), # ผูกเข้ากับฟังก์ชัน contact ใน views.py
+]
